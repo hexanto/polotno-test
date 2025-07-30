23dc019ca26158af40868dd5fe54d820bf4dbd6c
Louis <accounts@louislouw.me>
2025-07-30 12:15:24 +0200

    full editor

--- src/app/full/editor.tsx
+++ src/app/full/editor.tsx
@@ -1,3 +1,8 @@
+import { PolotnoContainer, SidePanelWrap, WorkspaceWrap } from "polotno";
+import { SidePanel } from "polotno/side-panel";
+import { Toolbar } from "polotno/toolbar/toolbar";
+import { ZoomButtons } from "polotno/toolbar/zoom-buttons";
+import { PagesTimeline } from "polotno/pages-timeline";
 import { Workspace } from "polotno/canvas/workspace";
 import { createStore } from "polotno/model/store";
 import data from "@/app/designs/d3.json";
@@ -26,15 +31,20 @@ export default function Editor() {
   );
 
   return (
-    <div style={{ width: "500px", height: "500px", display: "block" }}>
-      <Workspace
-        store={store}
-        pageControlsEnabled={true}
-        backgroundColor="#00FF00"
-        paddingX={0}
-        paddingY={0}
-        renderOnlyActivePage={true}
+    <PolotnoContainer style={{ width: "100vw", height: "100vh" }}>
+      <link
+        rel="stylesheet"
+        href="https://unpkg.com/@blueprintjs/core@5/lib/css/blueprint.css"
       />
-    </div>
+      <SidePanelWrap>
+        <SidePanel store={store} />
+      </SidePanelWrap>
+      <WorkspaceWrap>
+        <Toolbar store={store} downloadButtonEnabled />
+        <Workspace store={store} />
+        <ZoomButtons store={store} />
+        <PagesTimeline store={store} />
+      </WorkspaceWrap>
+    </PolotnoContainer>
   );
 }
