8944dd7bbcb82f48d93b00097ef3f77a470f4fb9
Louis <accounts@louislouw.me>
2025-07-30 13:51:05 +0200

    text change POC

--- src/app/poc/editor.tsx
+++ src/app/poc/editor.tsx
@@ -1,6 +1,8 @@
+import { observer } from "mobx-react-lite";
 import { Workspace } from "polotno/canvas/workspace";
 import { createStore } from "polotno/model/store";
 import data from "@/app/designs/d3.json";
+import { useEffect, useState } from "react";
 
 export default function Editor() {
   const store = createStore({
@@ -25,16 +27,58 @@ export default function Editor() {
     })
   );
 
+  // unsubscribe function
+  // const off = store.on("change", (e: any) => {
+  //   console.log("something changed", e);
+  // });
+
+  // useEffect(() => {
+  //   // unstable_setTextOverflow("change-font-size");
+  //   const el = store.activePage.children.find((x) => x.name == "team-name");
+  //   el.set({ text: teamName });
+  // }, [teamName]);
+
+  const Tooltip = () => {
+    return <div>Tooltip</div>;
+  };
+
+  const EditControls = observer(() => {
+    const elText = store.activePage.children.find((x) => x.name == "team-name");
+    return (
+      <div>
+        {elText && (
+          <input
+            type="text"
+            value={elText.text}
+            onChange={(e) => elText.set({ text: e.target.value })}
+          />
+        )}
+      </div>
+    );
+  });
+
+  useEffect(() => {
+    store.setScale(store.scaleToFit);
+  }, []);
+
   return (
-    <div style={{ width: "500px", height: "500px", display: "block" }}>
-      <Workspace
-        store={store}
-        pageControlsEnabled={true}
-        backgroundColor="#00FF00"
-        paddingX={0}
-        paddingY={0}
-        renderOnlyActivePage={true}
-      />
+    <div>
+      <div>
+        <EditControls />
+      </div>
+      <div style={{ width: "500px", height: "500px", display: "block" }}>
+        <Workspace
+          store={store}
+          pageControlsEnabled={true}
+          backgroundColor="#00FF00"
+          paddingX={0}
+          paddingY={0}
+          renderOnlyActivePage={true}
+          components={{
+            Tooltip: () => null,
+          }}
+        />
+      </div>
     </div>
   );
 }
